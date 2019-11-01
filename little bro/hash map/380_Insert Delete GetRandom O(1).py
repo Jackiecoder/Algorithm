2188140909b58ce8d178694572a98e0e942f0310
Junyi Zheng <zheng.junyi96@gmail.com>
2019-11-01 14:53:22 -0400

    Nov.1
    redo 380

--- little bro/hash map/380_Insert Delete GetRandom O(1).py
+++ little bro/hash map/380_Insert Delete GetRandom O(1).py
@@ -34,3 +34,48 @@ class RandomizedSet(object):
         :rtype: int
         """
         return random.choice(self.table.keys())
+
+# redo the O(1)
+
+class RandomizedSet(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.table = {}
+        self.ls = []
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val not in self.table.keys():
+            self.table[val] = len(self.ls)
+            self.ls.append(val)
+            return True
+        return False
+            
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        ind = self.table.pop(val, None)
+        if ind is None:
+            return False
+        key = self.ls.pop()
+        if len(self.ls)!=0 and len(self.ls) != ind:
+            self.ls[ind] = key
+            self.table[key] = ind
+        return True
+
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+        return random.choice(self.ls)
--- little bro/hash map/README.md
+++ little bro/hash map/README.md
@@ -1,6 +1,9 @@
 1. Question
     380 - O(1) time complexity
     Q: Does my code have O(1)?
+    A: hash map:  key --> list index -->  list value
+    Process: redo, runtime reduced from 800+ms to 600+ms. Although still beat 5% person, time complexity should be O(1)!
+
 
 2. Stared 
     18 - 4SUM
@@ -13,13 +16,16 @@
 4. Problem
     981 - Time Based Key-Value Store
     Time exceeded
+    A : find Max should be O(logN) binary search
 
 5. Problem
     139 - Use DP to break string by word dictionary
     Any method without DP?
+    A : ++++ recursion and memorization
 
 6. Problem 
     140 - hard version of 139 
     test case time exceed?
+    A : ++++ memorization
 "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
